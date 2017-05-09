9d47e664168bc10eefee5d13adf29e4c205b05fb
raphael prevost <raprevost@ma-3020-lx>
2017-05-09 10:12:53 +0200

    profil

--- monProfil.php
+++ monProfil.php
@@ -2,15 +2,32 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="Styles/domotique.css">
+        <link rel="stylesheet" href="Domotique.css">
 		<title>G7Entreprise</title>
     </head>
 
-<body>
-	<?php include'header2.php';?>
-	
+	<body>
+		<?php include'header2.php';?>
+	<h5> Prénom </h5>
+	<h5> Nom </h5>
+	<ul>
+		<div id="Adresse"> 
+			// image maison
+			<p> Adresse domicile 1 : 36 rue des longs prés </p>
+		</div>
+		<div id="Téléphone">
+			// image téléphone
+			<p> Téléphone : 06 06 06 06 06 </p>
+	</ul>
+	<div id="Autorisation gestion">
+		//image mec+
+		<p> Autoriser la gestion du domicile à un utilisateur
+	</div>
+	<div id="Ajout domicile">
+		//image ajout maison
+		<p> Ajouter un domicile </p>
+	</div>
+		<?php include 'footer.php' ?>
+	</body>
 
-	<?php include 'footer.php' ?>
-</body>
-
-</html>
+</html>	
